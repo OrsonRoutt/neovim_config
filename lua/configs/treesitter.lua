756e76be8cb4d763e6a3dd9a092b00cb426d9ec6
Orson Routt <103575527@student.swin.edu.au>
2025-08-15 04:18:51 +1000

    'treesitter-textobjects' Comment + Parameter Swap
    Added 'nvim-treesitter-textobjects' keybinds for comments and for
    swapping parameters.

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -28,27 +28,41 @@ return {
         ["if"] = { query = "@function.inner", desc = "inner function" },
         ["ac"] = { query = "@class.outer", desc = "class block" },
         ["ic"] = { query = "@class.inner", desc = "inner class" },
+        ["an"] = { query = "@comment.outer", desc = "comment" },
       },
     },
     move = {
       enable = true,
       set_jumps = true,
       goto_next_start = {
-        ["]m"] = { query = "@function.outer", desc = "Next function start" },
-        ["]]"] = { query = "@class.outer", desc = "Next class start" },
+        ["]m"] = { query = "@function.outer", desc = "next function start" },
+        ["]]"] = { query = "@class.outer", desc = "next class start" },
+        ["]n"] = { query = "@comment.outer", desc = "next comment start" },
       },
       goto_next_end = {
-        ["]M"] = { query = "@function.outer", desc = "Next function end" },
-        ["]["] = { query = "@class.outer", desc = "Next class end" },
+        ["]M"] = { query = "@function.outer", desc = "next function end" },
+        ["]["] = { query = "@class.outer", desc = "next class end" },
+        ["]N"] = { query = "@comment.outer", desc = "next comment end" },
       },
       goto_previous_start = {
-        ["[m"] = { query = "@function.outer", desc = "Previous function start" },
-        ["[["] = { query = "@class.outer", desc = "Previous class start" },
+        ["[m"] = { query = "@function.outer", desc = "previous function start" },
+        ["[["] = { query = "@class.outer", desc = "previous class start" },
+        ["[n"] = { query = "@comment.outer", desc = "previous comment start" },
       },
       goto_previous_end = {
-        ["[M"] = { query = "@function.outer", desc = "Previous function end" },
-        ["[]"] = { query = "@class.outer", desc = "Previous class end" },
+        ["[M"] = { query = "@function.outer", desc = "previous function end" },
+        ["[]"] = { query = "@class.outer", desc = "previous class end" },
+        ["[N"] = { query = "@comment.outer", desc = "previous comment end" },
       },
     },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = { query = "@parameter.inner", desc = "swap next parameter" },
+      },
+      swap_previous = {
+        ["<leader>A"] = { query = "@parameter.inner", desc = "swap previous parameter" },
+      },
+    }
   },
 }
