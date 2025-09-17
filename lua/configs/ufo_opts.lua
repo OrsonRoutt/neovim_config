9a2c88e5a546f929bd1580ad438437e85794f9de
Orson Routt <103575527@student.swin.edu.au>
2025-09-17 21:28:37 +1000

    Ufo Reasonable Fallbacks
    Made nvim-ufo do lsp --> treesitter --> indent instead of treesitter -->
    indent or lsp --> indent.

--- lua/configs/ufo_opts.lua
+++ lua/configs/ufo_opts.lua
@@ -1,6 +1,20 @@
+local function get_selector(bufnr)
+    local function handle_fallback_exception(err, provider_name)
+        if type(err) == "string" and err:match("UfoFallbackException") then
+            return require("ufo").getFolds(bufnr, provider_name)
+        else return require("promise").reject(err) end
+    end
+
+    return require("ufo").getFolds(bufnr, "lsp"):catch(function(err)
+        return handle_fallback_exception(err, "treesitter")
+    end):catch(function(err)
+        return handle_fallback_exception(err, "indent")
+    end)
+end
+
 return {
   open_fold_hl_timeout = 150,
-  provider_selector = function(_, _, _) return { "treesitter", "indent" } end,
+  provider_selector = function(_, _, _) return get_selector end,
   preview = {
     mappings = {
       scrollB = "",
